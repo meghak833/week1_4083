a34a61e88507c6f4068790066e9461c528102657
megha <megha.patel2@sheridancollege.ca>
2020-01-07 13:35:56 -0500

    first commit

---
+++ src/Syst17796/NameArray.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Syst17796;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Megha, Winter 2020
+ */
+public class NameArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        Scanner s=new Scanner(System.in);
+        
+    }
+    
+}
